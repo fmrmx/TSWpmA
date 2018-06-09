439b557ecf31a34bb1387830a783adbad75d5f71
fmrmx <fmr_@live.com>
2018-06-09 13:35:14 -0500

    Typos...

--- WBotEx.asm
+++ WBotEx.asm
@@ -3,9 +3,12 @@ void WBotEx(float Current_X_Degress_Address,float X_Degress_Address,float Curren
                 float My_Z_Location_Address,float Enemy_X_Location_Address,float Enemy_Y_Location_Address,
                 float Enemy_Z_Location_Address)
 	    
-            { My_X_Location,My_Y_Location,My_Z_Location,Enemy_X_Location,Enemy_Y_Location,Enemy_Z_Location } float Returns[12];
+{
+	float My_X_Location,My_Y_Location,My_Z_Location,Enemy_X_Location,Enemy_Y_Location,Enem​y_Z_Location,temp;
+	int Returns[12];
             
-            
+   __asm
+   {            
         Start:
 		      mov ecx,esp
 			  push [Get_If_Aiming_Address]
