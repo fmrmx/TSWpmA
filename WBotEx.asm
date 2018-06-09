be92e91b03942960e9d5784ed2cce9ea03622d72
Fmrmx <fmr_@live.com>
2018-06-09 13:07:01 -0500

    StofPr

---
+++ WBotEx.asm
@@ -0,0 +1,15 @@
+void WBotEx(
+                float Current_X_Degress_Address,
+                float X_Degress_Address,
+                float Current_Y_Degress_Address,
+                float Y_Degress_Address,
+                float Get_If_Aiming_Address,
+                float My_X_Location_Address,
+                float My_Y_Location_Address,
+                float My_Z_Location_Address,
+                float Enemy_X_Location_Address,
+                float Enemy_Y_Location_Address,
+                float Enemy_Z_Location_Address
+            )
+            { My_X_Location,My_Y_Location,My_Z_Location,Enemy_X_Location,Enemy_Y_Location,Enemy_Z_Location }
+            float Returns[12];
