2df2c6793df760f7b6a762378414dc468a9008f2
Fmrmx <fmr_@live.com>
2018-06-09 13:12:11 -0500

    corrections

--- WBotEx.asm
+++ WBotEx.asm
@@ -13,3 +13,45 @@ void WBotEx(
             )
             { My_X_Location,My_Y_Location,My_Z_Location,Enemy_X_Location,Enemy_Y_Location,Enemy_Z_Location }
             float Returns[12];
+            
+            
+        Start:
+		      mov ecx,esp
+			  push [Get_If_Aiming_Address]
+			  call ReadMemory
+			  fldz
+			  fld dword ptr [esp]
+			  fcom
+			  fstsw ax
+              sahf
+			  fstp dword ptr [esp-4]
+			  fstp dword ptr [esp-4]
+			  je end
+			  push [My_X_Location_Address]
+			  call ReadMemory
+			  pop eax
+			  push [My_Y_Location_Address]
+			  call ReadMemory
+			  pop edx
+			  push [My_Z_Location_Address]
+			  call ReadMemory
+			  pop ebx
+			  mov [My_X_Location],eax
+			  mov [My_Y_Location],edx
+			  mov [My_Z_Location],ebx
+			  push [Enemy_X_Location_Address]
+			  call ReadMemory
+			  pop eax
+			  push [Enemy_Y_Location_Address]
+			  call ReadMemory
+			  pop edx
+			  push [Enemy_Z_Location_Address]
+			  call ReadMemory
+			  pop ebx
+			  cmp eax,0
+			  je Testz
+			  jmp DontEnd
+              
+              
+              
+              
